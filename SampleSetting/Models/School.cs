9744c9166b43c6c9ce26334264208175c447568b
maeda <tankmaeda@gmail.com>
2018-08-26 16:01:06 +0900

    Enabled to diplay school districts.

---
+++ SampleSetting/Models/School.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleSetting.Models
+{
+    public class School
+    {
+        public string Name { get; set; }
+        public BuildingSize Size { get; set; }
+
+        public enum BuildingSize
+        {
+            Small,
+            Default,
+            Big
+        }
+    }
+}
---
+++ SampleSetting/Models/SchoolDistrict.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SampleSetting.Models
+{
+    public class SchoolDistrict
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<School> Schools { get; set; }
+    }
+}
